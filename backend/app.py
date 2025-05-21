f482e1597b97d8935ad34908e0042a0bfbb3c3c4
viictorperez <victor.perez.angulo@estudiantat.upc.edu>
2025-05-21 11:03:16 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from typing import List
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://viictorperez.github.io"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 data_store = []
 
