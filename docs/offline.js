df8eb68a186fb89b4adf1a6bbe14991f949dfdad
viictorperez <victor.perez.angulo@estudiantat.upc.edu>
2025-06-02 12:03:56 +0200

    Update offline.js

--- docs/offline.js
+++ docs/offline.js
@@ -8,7 +8,7 @@ const RANGO_MAXIMO = 300;
 document.getElementById('csvFile').addEventListener('change', function(evt) {
   Papa.parse(evt.target.files[0], {
     header: true,
-    dynamicTyping: true,
+    dynamicTyping: (field) => field !== 'timestamp',
     complete: function(resultados) {
       datos = resultados.data.filter(f => f.id !== undefined); // ignora líneas vacías
       indiceActual = 0;
